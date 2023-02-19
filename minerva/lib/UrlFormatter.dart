d8cc39b39ed2f04c29982d05076cae2472c31889
Ron Dubinsky <rondubinsky1@gmail.com>
2023-02-19 00:29:06 -0800

    Added UrlFormatter to convert from citations to FindlawUrls

---
+++ minerva/lib/UrlFormatter.dart
@@ -0,0 +1,9 @@
+
+const String FindlawUrlStem = "https://caselaw.findlaw.com/";
+const String FindlawScotusExtension = "us-supreme-court/"
+String getFindlawUrl(String citation) {
+  final List<String> splittedString = citation.split(' ');
+  return FindlawUrlStem + FindlawScotusExtension + splittedString[0] + '/'
+    + splittedString[2] + '.html';
+}
+
