95da3acc65392300a48a8ad530ae59da301b9a5b
Ron Dubinsky <rondubinsky1@gmail.com>
2023-02-18 18:54:00 -0800

    Fixed a bug related to uninitialized mutable variables

--- minerva/lib/MetaReadingPage.dart
+++ minerva/lib/MetaReadingPage.dart
@@ -2,6 +2,7 @@ import 'ReadingPage.dart';
 import 'CitationPage.dart';
 import 'SearchBar.dart';
 import 'dart:ui';
+import 'dart:core';
 import 'package:flutter/material.dart';
 import 'queryFunc.dart';
 
@@ -18,22 +19,27 @@ enum PageState { reading, citations }
 class MetaReadingPageState extends State<MetaReadingPage> {
   final String title;
   String searchValue = '';
-  var searchRes;
-  var chosenCase;
+  Map<String, dynamic> chosenCase = Map<String, dynamic>();
 
-  var currState = PageState.reading;
+  PageState currState = PageState.reading;
 
   MetaReadingPageState({required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold (
-      appBar: getSearchBar(searchValue, (value) => setState(() => searchValue = value), (value) async {
-       searchRes = await fetchSuggestions(value, "search");
-      return searchRes;
-      }, (value) {
-
-        currState = PageState.reading;}),
+      appBar: getSearchBar(
+        searchValue,
+        (value) => setState(() => searchValue = value),
+        (value) async {
+          var searchRes = await fetchSuggestions(value, "search")
+            ?? List.filled(0, "");
+          return searchRes;
+        }, 
+        (value) {
+          currState = PageState.reading;
+        }
+      ),
       drawer: getDrawer(),
       body:
       currState == PageState.reading ?
